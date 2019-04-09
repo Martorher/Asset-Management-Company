5cba27a81206b5da40ede28be68f078acbf30db6
Mariano Torres <42794973+Martorher@users.noreply.github.com>
2019-04-09 00:06:09 -0500

    Add files via upload

---
+++ Creacion_Tablas.sql
@@ -0,0 +1,89 @@
+﻿/*Creamos las diferenetes que conformaran la Base de Datos*/
+
+/*Creamo primero las tablas Empleado, Operador, Promotor*/
+
+CREATE TABLE "Empleados"(id_empleado SERIAL,
+	      nombre VARCHAR(50) NOT NULL,
+	      app VARCHAR(50) NOT NULL,
+	      apm VARCHAR(50) );
+
+CREATE TABLE "Operador"(id_operador SERIAL NOT NULL,
+			ced_oper VARCHAR(35) NOT NULL,
+			sueldo REAL NOT NULL,
+			id_empleado INTEGER NOT NULL);
+/*Se crea el atributo id_operador, para efectos de los registros, talvez estadistica de eficiencia de empledos análisis estadístico...*/
+CREATE TABLE "Promotor"(id_promotor SERIAL NOT NULL,
+			comision REAL NOT NULL,
+			id_empleado INTEGER NOT NULL);
+CREATE TABLE "Trade_off"(id_trade SERIAL,
+			isin VARCHAR(50),
+			unidades REAL NOT NULL,
+			precio REAL NOT NULL,
+			rendimiento REAL NOT NULL,
+			fecha_oper DATE NOT NULL,
+			monto REAL NOT NULL,
+			id_operacion INTEGER NOT NULL,
+			ced_oper VARCHAR(50) NOT NULL,
+			id_tipo_oper INTEGER NOT NULL,
+			id_instrumento INTEGER NOT NULL);
+			
+CREATE TABLE "Instrumento"(id_instrumento SERIAL NOT NULL,
+			nombre VARCHAR(50) NOT NULL,
+			tipo_valor VARCHAR(50) NOT NULL,
+			id_tipo_oper INTEGER NOT NULL,
+			id_emisora INTEGER NOT NULL);
+
+CREATE TABLE "Emisor"(id_emisora SERIAL,
+			nombre VARCHAR(100) NOT NULL,
+			sector VARCHAR(100) NOT NULL,
+			descripcion VARCHAR(150));
+			
+CREATE TABLE "Tipo_Operacion"(id_tipo_oper SERIAL NOT NULL,
+				tipo_oper VARCHAR(35) NOT NULL);
+				
+CREATE TABLE "Orden"(id_orden SERIAL,
+		asset_comision REAL,
+		inversion REAL NOT NULL,
+		P_G REAL NOT NULL,
+		monto REAL NOT NULL,
+		id_trade INTEGER NOT NULL,
+		isin VARCHAR(50),
+		id_instrumento INTEGER NOT NULL);
+		
+CREATE TABLE "Reporte" (id_reporte SERIAL,
+			estatus VARCHAR(100) NOT NULL,
+			tipo VARCHAR(100) NOT NULL,
+			monto REAL NOT NULL,
+			id_orden INTEGER NOT NULL);
+			
+CREATE TABLE "Detalle_Reporte"(id_inversionista INTEGER NOT NULL,
+		id_promotor INTEGER NOT NULL,
+		id_reporte INTEGER NOT NULL,
+		inversion REAL NOT NULL);
+		
+CREATE TABLE "Inversionista"(id_inversionista SERIAL NOT NULL,
+				rfc VARCHAR(50),
+				nombre_empresa VARCHAR(100) NOT NULL,
+				id_tipo_riesgo INTEGER NOT NULL);
+				
+CREATE TABLE "Tipo_Riesgo"(id_tipo_riesgo SERIAL,
+				categoria VARCHAR(100));
+				
+CREATE TABLE "Direccion_Fiscal" (id_direccion SERIAL NOT NULL,
+					calle VARCHAR(100) NOT NULL,
+					numero VARCHAR(100) NOT NULL,
+					colonia VARCHAR(100) NOT NULL,
+					cp VARCHAR(100) NOT NULL,
+					id_municipio INTEGER NOT NULL,
+					id_inverisionista INTEGER NOT NULL);
+					
+CREATE TABLE "Municipio"(id_municipio SERIAL NOT NULL,
+			municipio VARCHAR(100),
+			id_estado INTEGER NOT NULL);
+
+CREATE TABLE "Estado"(id_estado SERIAL NOT NULL,
+			estado VARCHAR(100) NOT NULL);
+			
+			
+	      
+	      
---
+++ PK_AND_FK.sql
@@ -0,0 +1,169 @@
+﻿--- Creacion de integridad de entidad (PK)
+
+ALTER TABLE "Estado"
+ADD CONSTRAINT estado_pk PRIMARY KEY (id_estado);
+
+ALTER TABLE "Municipio"
+ADD CONSTRAINT municipio_pk PRIMARY KEY (id_municipio);
+
+
+ALTER TABLE "Tipo_Riesgo"
+ADD CONSTRAINT tipo_riesgo_pk PRIMARY KEY (id_tipo_riesgo);
+
+
+ALTER TABLE "Inversionista"
+ADD CONSTRAINT inversionista_pk PRIMARY KEY (id_inversionista);
+
+
+ALTER TABLE "Direccion_Fiscal"
+ADD  CONSTRAINT direccion_fiscal_pk PRIMARY KEY (id_direccion);
+
+
+ALTER TABLE "Emisor"
+ADD CONSTRAINT emisor_pk PRIMARY KEY (id_emisora);
+
+
+ALTER TABLE "Tipo_Operacion"
+ADD CONSTRAINT tipo_operacion_pk PRIMARY KEY (id_tipo_oper);
+
+
+ALTER TABLE "Instrumento"
+ADD CONSTRAINT instrumento_pk PRIMARY KEY (id_instrumento);
+
+
+ALTER TABLE "Empleados"
+ADD CONSTRAINT empleado_pk PRIMARY KEY (id_empleado);
+
+
+ALTER TABLE "Promotor"
+ADD CONSTRAINT promotor_pk PRIMARY KEY (id_promotor);
+
+
+ALTER TABLE "Operador"
+ADD CONSTRAINT operador_pk PRIMARY KEY (ced_oper);
+
+ALTER TABLE "Trade_off"
+ADD CONSTRAINT trade_off_pk PRIMARY KEY (id_trade);
+
+ALTER TABLE "Orden"
+ADD CONSTRAINT orden_pk PRIMARY KEY (id_orden);
+
+
+ALTER TABLE "Reporte"
+ADD CONSTRAINT reporte_pk PRIMARY KEY (id_reporte);
+
+
+ALTER TABLE "Detalle_Reporte"
+ADD CONSTRAINT detalle_reporte_pk PRIMARY KEY (id_reporte);
+
+-- Creacion de integridad Referencial
+
+ALTER TABLE "Municipio" ADD CONSTRAINT estado_municipio_fk
+FOREIGN KEY (id_estado)
+REFERENCES "Estado" (id_estado)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+/*FK Tabla Direccion Fiscal*/
+ALTER TABLE "Direccion_Fiscal" ADD CONSTRAINT municipio_direccion_fiscal_fk
+FOREIGN KEY (id_municipio)
+REFERENCES "Municipio" (id_municipio)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Direccion_Fiscal" ADD CONSTRAINT inversionista_direccion_fiscal_fk
+FOREIGN KEY (id_inversionista)
+REFERENCES "Inversionista" (id_inversionista)
+ON UPDATE CASCADE ON DELETE CASCADE;
+/*#####################################################################*/
+
+ALTER TABLE "Inversionista" ADD CONSTRAINT tipo_riesgo_inversionista_fk
+FOREIGN KEY (id_tipo_riesgo)
+REFERENCES "Tipo_Riesgo" (id_tipo_riesgo)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+/*FK Tabla Detalle_Reporte*/
+ALTER TABLE "Detalle_Reporte" ADD CONSTRAINT inversionista_detalle_reporte_fk
+FOREIGN KEY (id_inversionista)
+REFERENCES "Inversionista" (id_inversionista)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Detalle_Reporte" ADD CONSTRAINT promor_detalle_reporte_fk
+FOREIGN KEY (id_promotor)
+REFERENCES "Promotor" (id_promotor)
+ON UPDATE CASCADE ON DELETE CASCADE;
+/*############################################################*/
+
+/*FK Tabla Instrumento*/
+ALTER TABLE "Instrumento" ADD CONSTRAINT emisor_instrumento_fk
+FOREIGN KEY (id_emisora)
+REFERENCES "Emisor" (id_emisora)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Instrumento" ADD CONSTRAINT tipo_operacion_instrumento_fk
+FOREIGN KEY (id_tipo_oper)
+REFERENCES "Tipo_Operacion" (id_tipo_oper)
+ON UPDATE CASCADE ON DELETE CASCADE;
+/*#######################################################*/
+
+ALTER TABLE "Operador" ADD CONSTRAINT empleado_operador_fk
+FOREIGN KEY (id_empleado)
+REFERENCES "Empleados" (id_empleado)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Promotor" ADD CONSTRAINT empleado_promotor_fk
+FOREIGN KEY (id_empleado)
+REFERENCES "Empleados" (id_empleado)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Detalle_Reporte" ADD CONSTRAINT promotor_detalle_reporte_fk
+FOREIGN KEY (id_promotor)
+REFERENCES "Promotor" (id_promotor)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+/*FK Tabla Trade_off */
+
+ALTER TABLE "Trade_off" ADD CONSTRAINT operador_trade_off_fk
+FOREIGN KEY (ced_oper)
+REFERENCES "Operador" (ced_oper)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Trade_off" ADD CONSTRAINT orden_trade_off_fk
+FOREIGN KEY (id_orden)
+REFERENCES "Orden" (id_orden)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+ALTER TABLE "Trade_off" ADD CONSTRAINT instrumento_trade_off_pk
+FOREIGN KEY (id_instrumento)
+REFERENCES "Instrumento" (id_instrumento)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Trade_off" ADD CONSTRAINT tipo_operacion_trade_off_fk
+FOREIGN KEY (id_tipo_oper)
+REFERENCES "Tipo_Operacion" (id_tipo_oper)
+ON UPDATE CASCADE ON DELETE CASCADE;
+/*####################################################*/
+
+/*FK Tabla Orden*/
+ALTER TABLE "Orden" ADD CONSTRAINT trade_off_instrumento_orden_fk
+FOREIGN KEY (isin, id_trade)
+REFERENCES "Trade_off" (isin, id_intrumento)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Orden" ADD CONSTRAINT instrumento_orden_fk
+FOREIGN KEY (id_instrumento)
+REFERENCES "Instrumento" (id_instrumento)
+ON UPDATE CASCADE ON DELETE CASCADE;
+ 
+
+/*#####################################################*/
+
+ALTER TABLE "Reporte" ADD CONSTRAINT orden_reporte_fk
+FOREIGN KEY (id_orden)
+REFERENCES "Orden" (id_orden)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "Detalle_Reporte" ADD CONSTRAINT reporte_detalle_reporte_fk
+FOREIGN KEY (id_reporte)
+REFERENCES "Reporte" (id_reporte)
+ON UPDATE CASCADE ON DELETE CASCADE;
